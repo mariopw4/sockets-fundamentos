ebf709aa926db98de08ae641304db8011b2b4271
Mario <mariopw4@gmail.com>
2020-06-13 13:21:46 -0500

    Primer commit

---
+++ public/js/socket.custom.js
@@ -0,0 +1,22 @@
+var socket = io();
+//Escuchar eventos
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+});
+
+socket.on('disconnect', function() {
+    console.log('Perdimos conexión con el servidor');
+});
+
+//Enviar información
+socket.emit('enviarMensaje', {
+    usuario: 'Mario',
+    mensaje: 'Hola Mundo'
+}, function(resp) {
+    console.log(resp);
+});
+
+//Escuchar información
+socket.on('enviarMensaje', function(mensaje) {
+    console.log(mensaje);
+});
